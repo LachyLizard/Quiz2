7a680668e63d90c40ceede4e92dff24f3033a8a4
LachyLizard <lachlanglarkin@gmail.com>
2026-06-09 10:24:14 +1200

    Intro & begin of arrays

---
+++ quiz.js
@@ -0,0 +1,21 @@
+//set score 0
+let score = 0;
+
+//arrary, object question answer >:3
+let quizArray = [
+    {
+        question: "",
+        answer: ""
+    },
+    {
+        
+    }]
+
+// Welcome user
+let name = prompt("Hello! What is you're name?");
+alert("Welcome to the ___ quiz " + name + "!");
+
+//For loop ask user questions
+//if question is empty [null] ask if they wish to retry the question
+
+//rating the quiz & giving score results, either as a % or a _/_
